dafa37cafa1a6daeb39fdfb5e81361d611625623
Sean Fleming <seenya@gmail.com>
2015-12-02 06:55:21 +1100

    First go

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,23 @@
-class teamcity inherits teamcity::params {
+class teamcity (
+		$cacheDir        = $teamcity::params::cacheDir,
+		$sourceTar       = $teamcity::params::sourceTar,
+		$destTar         = $teamcity::params::destTar,
+		$destUnzippedDir = $teamcity::params::destUnzippedDir,
+	) inherits teamcity::params {
 
 	file {$cacheDir :
 		ensure => 'directory',
+		#notify => Exec['unzip'],
 	}
 
 	file {$destTar :
 		source => $sourceTar
 	}
+
+#	exec { 'unzip':
+#		ensure => $destUnzippedDir
+#		command => "7z -x o${destDir} ${destUnzippedDir}",
+#		require => File[$destTar],
+#	}
 
 }
